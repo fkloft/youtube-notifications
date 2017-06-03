381113a027c5c335dce1e3c50226145422e580c6
Felix Kloft <felix.kloft@gmail.com>
2017-06-03 16:41:52 +0200

    add WebExtension

---
+++ background.js
@@ -0,0 +1,161 @@
+"use strict";
+
+function getYouTubeParams() {
+
+var result;
+	return fetch("https://www.youtube.com/", {
+		redirect: "follow",
+		credentials: "include",
+	})
+	.then(response => response.text())
+	.then(text => {
+		let index = text.indexOf("config['request-headers']");
+		if(index == -1) throw "string not found";
+		let start = text.indexOf("{", index);
+		if(start == -1) throw "string not found";
+		let end = text.indexOf("}", start);
+		if(end == -1) throw "string not found";
+		
+		let requestHeaders = JSON.parse(
+			text.substring(start, end + 1)
+			.replace(/\'/g, '"')
+		);
+		
+		let headers = new Headers();
+		for(let key in requestHeaders)
+			headers.append(key, requestHeaders[key]);
+		
+		index = text.indexOf("XSRF_TOKEN");
+		if(index == -1) throw "string not found";
+		start = text.lastIndexOf("{", index);
+		if(start == -1) throw "string not found";
+		end = text.indexOf("}", index);
+		if(end == -1) throw "string not found";
+		
+		
+		let config = JSON.parse(
+			text.substring(start, end + 1)
+			.replace(/\'/g, '"')
+		);
+		return {headers, config};
+	});
+}
+
+function getNotificationCount({headers, config}) {
+	return fetch('https://www.youtube.com/feed_ajax?action_get_unseen_notification_count=1', {
+		credentials: "include",
+		headers,
+	})
+	.then(response => response.json())
+	.then(obj => obj.unseen_notification_count);
+}
+
+function getNotifications({headers, config}) {
+	let body = new URLSearchParams();
+	body.append("session_token", config["XSRF_TOKEN"]);
+	body.append("action_get_notifications_flyout", "1");
+	
+	return fetch('https://www.youtube.com/feed_ajax?spf=load', {
+		method: 'POST',
+		body,
+		credentials: "include",
+		headers,
+	})
+	.then(response => response.json())
+	.then(obj => obj.body["yt-masthead-notifications-content"])
+	.then(parseNotifications);
+}
+
+function parseNotifications(text) {
+	let node = document.createElement("div");
+	node.innerHTML = text;
+	
+	return [...node.querySelectorAll("li .feed-item-container")].map(node => {
+		let unread = !!node.querySelector(".unread-dot");
+		let avatar = node.querySelector(".notification-avatar .yt-thumb img").src;
+		let link = node.querySelector(".yt-lockup-title a");
+		let title = link.title;
+		let url = link.href;
+		
+		let description = cleanup(node.querySelector(".yt-lockup-byline")).innerHTML;
+		let thumbnail = node.querySelector(".notification-thumb .yt-thumb img").src;
+		
+		return { unread, avatar, title, url, description, thumbnail };
+	});
+}
+
+function cleanup(node) {
+	while(node.attributes.length)
+		node.removeAttribute(node.attributes[0].name);
+	
+	for(let i = 0; i < node.children.length; i++) {
+		if(node.children[i].nodeName == "script") {
+			node.removeChild(node.children[i]);
+			i--;
+		} else {
+			cleanup(node.children[i]);
+		}
+	}
+	return node;
+}
+
+class Cache {
+	constructor(func, timeout = 15*60*1000) {
+		this.data = null;
+		this.lastUpdate = 0;
+		this.func = func;
+		this.timeout = timeout;
+	}
+	
+	getData() {
+		return new Promise((resolve, reject) => {
+			if(this.lastUpdate > Date.now() - this.timeout) {
+				resolve(this.data);
+				return;
+			}
+			
+			Promise.resolve(this.func())
+			.then(data => {
+				this.data = data;
+				this.lastUpdate = Date.now();
+				resolve(data);
+			})
+			.catch(reject);
+		});
+	}
+}
+
+function setup(data) {
+	setInterval(() => updateBadge(data), 600000);
+	updateBadge(data);
+	
+	let cache = new Cache(() => getNotifications(data));
+	
+	browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		if(request.type == "getNotifications") {
+			cache.getData().then(sendResponse);
+			return true;
+		}
+	});
+}
+
+function updateBadge(data) {
+	getNotificationCount(data)
+	.then(count => {
+		if(count == 0) {
+			browser.browserAction.setBadgeText({text: ""});
+			browser.browserAction.setTitle({title: "YouTube"});
+		} else {
+			browser.browserAction.setBadgeText({text: "" + count});
+			browser.browserAction.setTitle({title: "YouTube: " + count + " unread notification(s)"});
+		}
+	});
+}
+
+let base = document.head.appendChild(document.createElement("base"));
+base.href = "https://youtube.com/";
+
+getYouTubeParams()
+.then(setup);
+
+
---
+++ popup.js
@@ -0,0 +1,93 @@
+"use strict";
+
+let list = document.querySelector("ol");
+
+browser.runtime.sendMessage({
+	type: "getNotifications",
+})
+.then(i=>new Promise(resolve => setTimeout(() => resolve(i), 5000)))
+.then(notifications => {
+	list.innerHTML = "";
+	list.className = "";
+	
+	notifications.forEach(notification => {
+		let item = list.appendChild(document.createElement("li"));
+		if(notification.unread)
+			item.className = "unread notification";
+		else
+			item.className = "notification";
+		
+		let link = item.appendChild(document.createElement("a"));
+		link.href = notification.url;
+		
+		let avatar = link.appendChild(document.createElement("img"));
+		avatar.className = "avatar";
+		avatar.src = notification.avatar;
+		
+		let content = link.appendChild(document.createElement("div"));
+		content.className = "content";
+		
+		let title = content.appendChild(document.createElement("h1"));
+		title.className = "title";
+		title.textContent = notification.title;
+		
+		let desc = content.appendChild(document.createElement("ul"));
+		desc.className = "description";
+		desc.innerHTML = notification.description;
+		
+		let thumb = link.appendChild(document.createElement("div"));
+		thumb.className = "thumbnail";
+		
+		let img = thumb.appendChild(document.createElement("img"));
+		img.src = notification.thumbnail;
+	});
+})
+.catch(e => {
+	list.innerHTML = "<li>An error has occured, could not load notifications.</li>";
+	list.className = "error";
+	throw e;
+});
+
+function findLink(event) {
+	let element = event.target;
+	do {
+		if(element.nodeName.toLowerCase() == "a")
+			return element;
+	} while(element = element.parentNode);
+	return null;
+}
+
+document.addEventListener("click", ev => {
+	let link = findLink(ev);
+	if(!link) return;
+	
+	if(ev.which > 2) return;
+	
+	ev.preventDefault();
+	
+	let properties = {
+		url: link.href,
+	};
+	
+	if(ev.which == 2 || ev.ctrlKey) {
+		// new tab
+		properties.active = !ev.shiftKey; // background if with shift
+		browser.tabs.create(properties)
+		.then(() => close());
+	} else if(ev.shiftKey) {
+		// new window
+		browser.windows.create(properties)
+		.then(() => close());
+	} else {
+		// current tab or new tab
+		browser.tabs.query({
+			active: true,
+			currentWindow: true,
+		})
+		.then(tabs => tabs[0].id)
+		.then(tabId => browser.tabs.update(tabId, properties))
+		.catch(e => browser.tabs.create(properties))
+		.then(() => close());
+	}
+});
+
